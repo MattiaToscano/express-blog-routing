1fb03ca9ef096a46d40121524db9622a58f50eea
MattiaToscano <mattia1997ct@gmail.com>
2025-04-17 13:50:55 +0200

    [UPD] Definito porta e prima rotta, inserita cartella e file

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const express = require('express');
 
 //Inizializzo express
 const app = express();
+
+//Definisco la porta
+const port = 3000;
+
+//Definisco la rotta
+app.get('/', (req,res) =>{
+    res.send('Mainpage')
+
+})
