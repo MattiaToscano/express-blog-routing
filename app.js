cf89ee08038464767b125d826b16480ed10fcf2d
MattiaToscano <mattia1997ct@gmail.com>
2025-04-17 13:56:40 +0200

    [UPD] Creata cartella router e file all'interno

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ const port = 3000;
 //Definisco la rotta
 app.get('/', (req,res) =>{
     res.send('Mainpage')
+})
 
+//Faccio rimanere il server in ascolto
+app.listen(port,()=>{
+    console.log(`Il server è in ascolto sulla porta ${port}`)
 })
---
+++ routing/posts.js
@@ -0,0 +1,7 @@
+//Importo express
+const express =require('express');
+
+//Definisco il router
+const router = express.Router();
+
+//Riprendo l'array
